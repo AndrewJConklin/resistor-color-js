638bd7306290172ae0a1c1630a89ad72df8e2a9d
aconkli9 <aconkli9@ford.com>
2022-04-25 15:02:32 -0400

    Solve first question

--- resistor-color.js
+++ resistor-color.js
@@ -1,3 +1,34 @@
-export function colorCode() {}
+export function colorCode(color) {
+    if (color.includes("black")) {
+        return 0
+    }
+    if (color.includes("brown")) {
+        return 1
+    }
+    if (color.includes("red")) {
+        return 2
+    }
+    if (color.includes("orange")) {
+        return 3
+    }
+    if (color.includes("yellow")) {
+        return 4
+    }
+    if (color.includes("green")) {
+        return 5
+    }
+    if (color.includes("blue")) {
+        return 6
+    }
+    if (color.includes("violet")) {
+        return 7
+    }
+    if (color.includes("grey")) {
+        return 8
+    }
+    if (color.includes("white")) {
+        return 9
+    }
+}
 
 export const COLORS = [];
